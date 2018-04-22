1528520f53d554f25e872fc70a71a436193582b6
Isaias Pomales <impomales@gmail.com>
2018-04-21 20:35:45 -0400

    index html renders with random tweets

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
 const express = require('express');
 const chalk = require('chalk');
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
 const app = express();
-
-const data = {
-  title: 'An example',
-  people: [
-    { name: 'Isaias' },
-    { name: 'Mark' },
-    { name: 'Sarah' }
-  ]
-};
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render)
 nunjucks.configure('views', { noCache: true });
+
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
   console.log(chalk.green(req.method), chalk.blue(req.path));
   next();
 });
 
-app.get('/', (req, res) => {
-  res.render('index', data);
-});
-
-app.get('/news', (req, res) => {
-  res.send('in other news...\n');
-});
+app.use('/', routes);
 
 app.listen(3000, () => {
   console.log('server listening');
---
+++ tweetBank.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+
+const data = [];
+
+const randArrayEl = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const getFakeName = function() {
+  const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
+  const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+};
+
+const getFakeTweet = function() {
+  const awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing', 'impressive'];
+  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+};
+
+module.exports = {
+  add: function(name, content) {
+    data.push({ name, content })
+  },
+  list: function() {
+    return _.cloneDeep(data);
+  },
+  find: function(properties) {
+    return _.cloneDeep(_.filter(data, properties));
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  module.exports.add( getFakeName(), getFakeTweet() );
+}
+
+console.log(data);
