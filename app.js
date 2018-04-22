40e1d196e585978734dc957fceb06083961147c1
Isaias Pomales <impomales@gmail.com>
2018-04-21 20:16:14 -0400

    nunjucks template complete

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const chalk = require('chalk');
 const nunjucks = require('nunjucks');
 const app = express();
 
+const data = {
+  title: 'An example',
+  people: [
+    { name: 'Isaias' },
+    { name: 'Mark' },
+    { name: 'Sarah' }
+  ]
+};
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render)
+nunjucks.configure('views', { noCache: true });
+
 app.use((req, res, next) => {
   console.log(chalk.green(req.method), chalk.blue(req.path));
   next();
 });
 
 app.get('/', (req, res) => {
-  res.send('hello world!\n');
+  res.render('index', data);
 });
 
 app.get('/news', (req, res) => {
